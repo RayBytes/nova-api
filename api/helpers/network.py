ea68a638e9d7264a55ba5213efb61d5d0f7248f2
nsde <neostyxde@gmail.com>
2023-08-12 17:53:54 +0200

    Removed unneccesary code

--- api/helpers/network.py
+++ api/helpers/network.py
@@ -17,20 +17,3 @@ async def get_ip(request) -> str:
     detected_ip = next((i for i in possible_ips if i), None)
 
     return detected_ip
-
-async def add_proxy_auth_to_headers(username: str, password: str, headers: dict) -> dict:
-    """Add proxy authentication to the headers. This is useful if the proxy authentication doesn't work as it should."""
-
-    proxy_auth = base64.b64encode(f'{username}:{password}'.encode()).decode()
-    headers['Proxy-Authorization'] = f'Basic {proxy_auth}'
-    return headers
-
-if __name__ == '__main__':
-    print(asyncio.run(add_proxy_auth_to_headers(
-        'user',
-        'pass',
-        {
-            'Authorization': 'Bearer demo',
-            'Another-Header': '123'
-        }
-    )))
